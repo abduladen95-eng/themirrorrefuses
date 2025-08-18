23835ca706a3ef31638fc64a850b562858ad15e8
abduladen95-eng <abdul.aden95@gmail.com>
2025-08-17 22:47:10 -0400

    Delete scream.js

--- scream.js
+++
@@ -1,7 +0,0 @@
-function activateScream() {
-    const status = document.getElementById("screamStatus");
-    status.innerHTML = "⚠️ SCREAM.EXE IS RUNNING...";
-    setTimeout(() => {
-        status.innerHTML = "🩸 THE MIRROR HAS REFUSED. TWUSSEH FEEDS.";
-    }, 3000);
-}
